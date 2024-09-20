85affff489891b8dcc8fcf04b51a189e45c6410c
arturgawlik <artur.gawlik@icloud.com>
2024-09-20 21:33:47 +0200

    init

---
+++ custom-element.js
@@ -0,0 +1,97 @@
+import { LitElement, html, css, isServer } from "lit";
+// import {
+//   LitElement,
+//   html,
+//   css,
+// } from "https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js";
+
+const customElementTmpl = ({
+  hideGreeting,
+  customText,
+  onInput,
+  inputValue,
+  onThemeChange,
+  onGreetingChange,
+  setInputToDefault,
+}) => html`
+  <p ?hidden="${hideGreeting}">hello from my custom element!</p>
+  <p>${customText}</p>
+  <input @input=${onInput} .value=${inputValue} />
+  <div>
+    <label>
+      dark theme:
+      <input type="checkbox" @change=${onThemeChange} />
+    </label>
+  </div>
+  <div>
+    <label>
+      hide greeting:
+      <input type="checkbox" @change=${onGreetingChange} />
+    </label>
+  </div>
+  <div>
+    <button @click=${setInputToDefault}>set input to default</button>
+  </div>
+`;
+
+class CustomElement extends LitElement {
+  static properties = {
+    customText: {},
+    theme: {},
+    hideGreeting: {},
+    inputValue: {},
+  };
+
+  static styles = css`
+    p {
+      margin: 30px;
+    }
+  `;
+
+  constructor() {
+    super();
+    this.customText = "";
+    this.theme = "light";
+    this.hideGreeting = false;
+    this.inputValue = "";
+  }
+
+  #onInput(event) {
+    const input = event.target;
+    this.customText = input.value;
+    this.inputValue = input.value;
+  }
+
+  #onThemeChange(event) {
+    const input = event.target;
+    this.theme = input.checked ? "dark" : "light";
+    this.#updateTheme();
+  }
+
+  #updateTheme() {
+    document.documentElement.style.colorScheme = this.theme;
+  }
+
+  #onGreetingChange(event) {
+    const input = event.target;
+    this.hideGreeting = input.checked ? true : false;
+  }
+
+  #setInputToDefault() {
+    this.customText = "default input value";
+    this.inputValue = "default input value";
+  }
+
+  render() {
+    return customElementTmpl({
+      customText: this.customText,
+      hideGreeting: this.hideGreeting,
+      inputValue: this.inputValue,
+      setInputToDefault: this.#setInputToDefault,
+      onGreetingChange: this.#onGreetingChange,
+      onInput: this.#onInput,
+      onThemeChange: this.#onThemeChange,
+    });
+  }
+}
+customElements.define("custom-element", CustomElement);
---
+++ server.js
@@ -0,0 +1,90 @@
+import { createServer } from "node:http";
+import { render } from "@lit-labs/ssr";
+import { html } from "lit";
+import { createReadStream } from "node:fs";
+import { pipeline } from "node:stream";
+import { unsafeHTML } from "lit/directives/unsafe-html.js";
+
+import "./custom-element.js";
+import { collectResult } from "@lit-labs/ssr/lib/render-result.js";
+
+createServer((req, res) => {
+  try {
+    const fileUrl = getFileUrl(req);
+    const contentType = getContentType(fileUrl);
+    const fileStream = createReadStream(fileUrl);
+
+    res.writeHead(200, {
+      "content-type": contentType,
+    });
+
+    if (isIndexHtml(fileUrl)) {
+      handleIndexHtml(fileStream, res);
+    } else {
+      handleOtherFiles(fileStream, res);
+    }
+  } catch (err) {
+    console.error(err);
+    res.end("error: ", err);
+  }
+}).listen(8000, () => console.log("server running http://localhost:8000"));
+
+function getFileUrl(req) {
+  return `.${req.url === "/" ? "/index.html" : req.url}`;
+}
+
+function getContentType(filePath) {
+  const extension = getExtension(filePath);
+  switch (extension) {
+    case "js":
+      return "application/javascript";
+    case "html":
+      return "text/html";
+    default:
+      return "text/plain";
+  }
+}
+
+function getExtension(filePath) {
+  if (filePath.includes(".")) {
+    const [, , extension] = filePath.split(".");
+    return extension;
+  }
+}
+
+function isIndexHtml(fileUrl) {
+  return fileUrl.includes("index.html");
+}
+
+function handleIndexHtml(fileStream, res) {
+  pipeline(
+    fileStream,
+    async function* (source) {
+      source.setEncoding("utf8");
+
+      for await (const chunk of source) {
+        const litTmpl = html`${unsafeHTML(chunk)}`;
+        const renderRes = render(litTmpl);
+        yield await collectResult(renderRes);
+      }
+    },
+    res,
+    handlePiplineError(res)
+  );
+}
+
+function handleOtherFiles(fileSource, res) {
+  pipeline(fileSource, res, handlePiplineError(res));
+}
+
+function handlePiplineError(res) {
+  return function (err) {
+    if (err) {
+      res.end("error");
+      console.log("pipeline failed, error: ");
+      console.error(err);
+    } else {
+      console.log("pipline successed");
+    }
+  };
+}
